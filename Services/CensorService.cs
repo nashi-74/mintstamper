dac65cfd0a5d7b229615def0ca1090e2e52cc7a5
virusek20 <gmoder.virusek20@gmail.com>
2024-12-24 01:12:57 +0100

    Split code into components and services

---
+++ Services/CensorService.cs
@@ -0,0 +1,36 @@
+﻿using System.Text.RegularExpressions;
+
+namespace mintstamper.Services;
+
+public interface ICensorService
+{
+    string CensorText(string text, List<string> replacements);
+}
+
+public class CensorService : ICensorService
+{
+    public string CensorText(string text, List<string> replacements)
+    {
+        foreach (var replacement in replacements)
+        {
+            string pattern = $@"\b{Regex.Escape(replacement)}\b";
+            text = Regex.Replace(text, pattern, match => CensorWord(match.Value), RegexOptions.IgnoreCase);
+        }
+
+        return text;
+    }
+
+    private static string CensorWord(string word)
+    {
+        foreach (char c in word)
+        {
+            if ("aeiouAEIOU".Contains(c))
+            {
+                int censorIndex = word.IndexOf(c);
+                return word.Remove(censorIndex, 1).Insert(censorIndex, "*");
+            }
+        }
+
+        return word.Remove(0, 1).Insert(0, "*");
+    }
+}
---
+++ Stamp.cs
@@ -0,0 +1,17 @@
+﻿namespace mintstamper;
+
+public enum StampTypeEnum
+{
+    Regular,
+    Control,
+    Note,
+    Stacked
+}
+
+public record Stamp
+(
+    string Text,
+    StampTypeEnum StampType,
+    bool IsBirthday,
+    int TimestampSeconds
+);
