9a5090f40c5b72a97706114f1eb2df455ac0eeb4
nsh74 <n4rvix@gmail.com>
2025-01-20 06:21:32 +0100

    Basic conversion settings added

---
+++ Models/BreakSettings.cs
@@ -0,0 +1,14 @@
+﻿namespace mintstamper.Models
+{
+    public partial class BreakSettings
+    {
+        public string? Interval { get; set; }
+        public bool Enabled { get; set; }
+
+        public BreakSettings(string? interval, bool enabled)
+        {
+            Interval = interval;
+            Enabled = enabled;
+        }
+    }
+}
